445971e2f724475faa8705d3c59f76cb3174a43a
DJDrama <putzzang@hotmail.com>
2020-10-21 14:26:44 +0900

    Tower Of Hanoi

---
+++ 3_Recursion/8_TowerOfHanoi.java
@@ -0,0 +1,24 @@
+public class TowerOfHanoi {
+    void printTowerOfHanoi(int n, char from, char extra, char to){
+        if(n==1){
+            System.out.println("Move 1 from " + from + " to " + to);
+            return;
+        }
+        printTowerOfHanoi(n-1, from, to, extra);
+        System.out.println("Move " + n+ " from " + from + " to " + to);
+        printTowerOfHanoi(n-1, extra, from, to);
+    }
+}
+
+/**
+ * Given a string, point all subsets of it in any order
+ */
+class Main {
+    public static void main(String[] args) {
+        TowerOfHanoi t = new TowerOfHanoi();
+
+        int n=3;
+        t.printTowerOfHanoi(n, 'A', 'B', 'C');
+
+    }
+}
