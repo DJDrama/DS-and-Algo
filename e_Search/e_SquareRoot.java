25e00a0c668d11a60e0cdc7eaf5c6fbff115dbe0
DJDrama <putzzang@hotmail.com>
2020-11-02 20:45:44 +0900

    Modified for Integer OverFlow

--- e_Search/e_SquareRoot.java
+++ e_Search/e_SquareRoot.java
@@ -1,14 +1,13 @@
 public class SquareRoot {
     int getSquareRoot(int n) {
         int res = 0;
-        int l = 1; // root(0) == 0
+        int l = 1;
         int r = n;
         while (l <= r) {
-            int mid = l + r / 2;
-            int mult = mid * mid;
-            if (mult == n)
+            int mid = (l+r) / 2;
+            if (mid == n/mid)
                 return mid;
-            else if (n < mult)
+            else if (n/mid < mid)
                 r = mid - 1;
             else {
                 l = mid + 1;
