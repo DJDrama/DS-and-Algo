6196a2d93a900bc3e3011afc73e3376a5e9f7335
DJDrama <putzzang@hotmail.com>
2020-12-03 16:28:26 +0900

    Bellman Ford Algorithm

---
+++ q_Graph/s_BellmanFordAlgorithm.java
@@ -0,0 +1,90 @@
+import java.util.Arrays;
+
+// Works for negative weight
+public class BellmanFordAlgorithm {
+    class Edge {
+        int src, dest, weight;
+    }
+
+    int V, E;
+    Edge edge[];
+
+    BellmanFordAlgorithm(int v, int e) {
+        V = v;
+        E = e;
+        edge = new Edge[e];
+        for (int i = 0; i < e; i++)
+            edge[i] = new Edge();
+    }
+
+    void BellmanFord(BellmanFordAlgorithm graph, int src) {
+        int[] dist = new int[V];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
+
+        for (int i = 1; i < V; i++) {
+            for (int j = 0; j < E; j++) {
+                int u = graph.edge[j].src;
+                int v = graph.edge[j].dest;
+                int weight = graph.edge[j].weight;
+                if (dist[u] != Integer.MAX_VALUE && dist[u] + weight < dist[v])
+                    dist[v] = dist[u] + weight;
+            }
+        }
+
+        // check more and if there is more negative number, it means there is a negative cycle.
+        for (int j = 0; j < E; j++) {
+            int u = graph.edge[j].src;
+            int v = graph.edge[j].dest;
+            int weight = graph.edge[j].weight;
+            if (dist[u] != Integer.MAX_VALUE && dist[u] + weight < dist[v]) {
+                System.out.println("Graph contains negative weight cycle!");
+                return;
+            }
+        }
+        printArr(dist);
+    }
+
+    void printArr(int[] dist) {
+        System.out.println("Vertex distance from source");
+        for (int i = 0; i < V; i++)
+            System.out.println(i + "\t\t" + dist[i]);
+    }
+
+
+}
+
+class Main {
+    public static void main(String[] args) {
+        int V = 4;
+        int E = 5;
+        BellmanFordAlgorithm graph = new BellmanFordAlgorithm(V, E);
+
+        // add edge 0-1 (or A-B)
+        graph.edge[0].src = 0;
+        graph.edge[0].dest = 1;
+        graph.edge[0].weight = 1;
+
+        // add edge 0-2 (or A-C)
+        graph.edge[1].src = 0;
+        graph.edge[1].dest = 2;
+        graph.edge[1].weight = 4;
+
+        // add edge 1-2 (or B-C)
+        graph.edge[2].src = 1;
+        graph.edge[2].dest = 2;
+        graph.edge[2].weight = -3;
+
+        // add edge 1-3 (or B-D)
+        graph.edge[3].src = 1;
+        graph.edge[3].dest = 3;
+        graph.edge[3].weight = 2;
+
+        // add edge 2-3 (or C-D)
+        graph.edge[4].src = 2;
+        graph.edge[4].dest = 3;
+        graph.edge[4].weight = 3;
+
+        graph.BellmanFord(graph, 0);
+    }
+}
